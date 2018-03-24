cef29c3d61144171605b0a0766fe5b35a53bc1ad
Josh Pierro <josh_pierro@fsafood.com>
2018-03-23 21:38:21 -0700

    stub out routes

--- yield-calculator/src/app/app.component.ts
+++ yield-calculator/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Event, Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private currentRoute: Router;
+  public path: string;
+
+  constructor(private route: Router) {
+    this.currentRoute = route;
+  }
+
+  ngOnInit() {
+    this.currentRoute.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.path = event.url;
+      }
+    });
+
+  }
 }
--- yield-calculator/src/app/app.module.ts
+++ yield-calculator/src/app/app.module.ts
@@ -1,19 +1,37 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {MatCardModule,MatToolbarModule} from "@angular/material";
 import {NgModule} from '@angular/core';
-
+import {RouterModule,Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
+
+const appRoutes: Routes = [
+  { path: '',
+    component: SettingsComponent,
+    data: { title: 'Home' },
+  },
+  { path: 'settings',
+    component: SettingsComponent,
+    data: { title: 'Settings' }
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     MatCardModule,
     MatToolbarModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: false }),
   ],
   providers: [],
   bootstrap: [AppComponent]
